3eb0107f929216442cb2f4cb921fdb38eb6cfc42
Angel <angel.gtz.pc.527@gmail.com>
2024-05-16 19:52:12 -0600

    Check

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    {
-      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
-      useValue: {
-        subscriptSizing: 'dynamic'
-      }
-    }
-  ],
-});
-
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
